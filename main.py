3f6d619617f5d1531bdb2fc89d1c137c004d6264
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-12-01 03:44:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from pytgcalls import idle
+
+from config import call_py
+from EvaMaria.quote import arq
+
+
+async def main():
+    await call_py.start()
+    print(
+        """
+    ------------------
+   | EvaMaria Started! |
+    ------------------
+"""
+    )
+    await idle()
+    await arq.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
