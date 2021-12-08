39696a9b58c1fce93a324ccaea55576b66cff608
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-12-08 17:22:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_ID = int(os.getenv("API_ID", "5838321"))
 API_HASH = os.getenv("API_HASH", "b31649ee8513ccc7f27468389bf1b362")
 SESSION = os.getenv("SESSION", "BQBOqNlOEUZlWURs7bENUmslCGLOa18CqIdEfg9mDOPNRKAfGFeqPZb4Lp-x1Bkt31zemSkvDamrs9A3ooZjllLLgm5oKaDi89_owlgKHh-jBvz2fpJsl1J6nxELSl-MrGeuP_oKNk7I_bksPXMVnVoNPXvLGhQ98V9J9Nbyo18_62w-DkO77oDrXSxOjBcWixzfm-spOe45EXSxgEXhwvP1ixtWwjvJ3smzYqnI1UA9icNe1TxU-B-jkzy8TrmCJb8y0izgm76GOT6dOhjn1JEQgtW49urLnOLuwVvK0uOMwovypmzOBJUZfh9KMaKiGHJL6mm44iKT5Ld4euXUPSbUYtbrYwA")
 HNDLR = os.getenv("HNDLR", "")
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "1658252131").split()))
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
 
 contact_filter = filters.create(
