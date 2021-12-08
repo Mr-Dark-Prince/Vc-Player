ea2002fbf9235d1411a055d9c0cb30ba58a780fd
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-12-08 19:12:49 +0530

    Create ping.py

---
+++ EvaMaria/ping.py
@@ -0,0 +1,63 @@
+import os
+import sys
+from datetime import datetime
+from time import time
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import HNDLR, SUDO_USERS
+
+# System Uptime
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (
+    ('Week', 60 * 60 * 24 * 7),
+    ('Day', 60 * 60 * 24),
+    ('Hour', 60 * 60),
+    ('Min', 60),
+    ('Sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else ""))
+    return ", ".join(parts)
+
+
+@Client.on_message(filters.command(["ping"], prefixes=f"{HNDLR}"))
+async def ping(client, m: Message):
+    await m.delete()
+    start = time()
+    current_time = datetime.utcnow()
+    m_reply = await m.reply_text("⚡")
+    delta_ping = time() - start
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m_reply.edit(
+        f"<b>I'm Online🍀</b> `{delta_ping * 1000:.3f} ms` \n<b>⏳Uptime </b> - `{uptime}`"
+    )
+
+
+@Client.on_message(
+    filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=f"{HNDLR}")
+)
+async def restart(client, m: Message):
+    await m.delete()
+    loli = await m.reply("1")
+    await loli.edit("2")
+    await loli.edit("3")
+    await loli.edit("4")
+    await loli.edit("5")
+    await loli.edit("6")
+    await loli.edit("7")
+    await loli.edit("8")
+    await loli.edit("9")
+    await loli.edit("**🖥️System🖱️Restarted⌨️**")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
+
