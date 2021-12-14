78e52c9d3300930295dc0c09e5cf138aea8afd99
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-14 15:19:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-API_ID = int(os.getenv("API_ID", "5838321"))
-API_HASH = os.getenv("API_HASH", "b31649ee8513ccc7f27468389bf1b362")
-SESSION = os.getenv("SESSION", "BQBOqNlOEUZlWURs7bENUmslCGLOa18CqIdEfg9mDOPNRKAfGFeqPZb4Lp-x1Bkt31zemSkvDamrs9A3ooZjllLLgm5oKaDi89_owlgKHh-jBvz2fpJsl1J6nxELSl-MrGeuP_oKNk7I_bksPXMVnVoNPXvLGhQ98V9J9Nbyo18_62w-DkO77oDrXSxOjBcWixzfm-spOe45EXSxgEXhwvP1ixtWwjvJ3smzYqnI1UA9icNe1TxU-B-jkzy8TrmCJb8y0izgm76GOT6dOhjn1JEQgtW49urLnOLuwVvK0uOMwovypmzOBJUZfh9KMaKiGHJL6mm44iKT5Ld4euXUPSbUYtbrYwA")
+API_ID = int(os.getenv("API_ID", "17755999"))
+API_HASH = os.getenv("API_HASH", "505b6fef2e96f37dabee8e1115af8e63")
+SESSION = os.getenv("SESSION", "AQA-F-NhnJx2u26vbLp1KJSVFPMkrEMHudN8BipoUoP__ZXSh76hE-td76yjZ0fTQsF_D6TbN1msD-MedJtPJaoaDk1Vgw-SGv1UEXL3ugcAvn2dsYQDHptRiWM_tww3hLVn_GONjK4ABeoIpnPzoOfMX_ObdFv7c-L67h3Yts51SHbDfz_kNS8_7lsspJ0RgTqJ80m7tpJB__4YdMiL3mz3t4DebHPz-PUt-wUcrAIvb9izeOPq35aq51jKmEv5e-7RV9bi6yQKfCKWCeOM7Snk0jV4Mea3oqnslkcHLnjtsk42ckr9ibOHDU-wpYeMec7K7kCZG6smwn4CpzqDBwpUAAAAASseFXgA")
 HNDLR = os.getenv("HNDLR", "")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
