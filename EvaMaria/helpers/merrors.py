b03fd11978be19bd4e9b41b6c40dd87b187e8b45
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-22 00:48:21 +0530

    Update merrors.py

--- EvaMaria/helpers/merrors.py
+++ EvaMaria/helpers/merrors.py
@@ -1,7 +1,6 @@
 import sys
 import traceback
 from functools import wraps
-from EvaMaria import app
 
 from pyrogram import Client
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
@@ -50,7 +49,7 @@ def capture_err(func):
                 ),
             )
             for x in error_feedback:
-                await app.send_message(-1001739603420, x)
+                await client.send_message(-1001739603420, x)
             raise err
 
     return capture
