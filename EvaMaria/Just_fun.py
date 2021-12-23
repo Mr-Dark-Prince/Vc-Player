10feacc6396539058a5a93aaf73127fe657ad522
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-24 04:22:17 +0530

    Update Just_fun.py

---
+++ EvaMaria/Just_fun.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+
+
+DART_E_MOJI = "🎯"
+
+
+@Client.on_message(filters.command(["throw", "dart"], prefixes=f"{HNDLR}"))
+async def throw_dart(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=DART_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+
+
+DICE_E_MOJI = "🎲"
+
+
+@Client.on_message(filters.command(["dice", "roll"], prefixes=f"{HNDLR}"))
+async def roll_dice(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=DICE_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
