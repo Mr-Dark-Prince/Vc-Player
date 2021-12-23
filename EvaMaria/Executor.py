59f0cbd98ddbcd3be962cee024e81dfb0209b2f7
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-23 13:26:06 +0530

    Create Executor.py

---
+++ EvaMaria/Executor.py
@@ -0,0 +1,11 @@
+
+import sys
+import io
+import keyword
+import traceback
+import asyncio
+
+from getpass import getuser
+from os import geteuid
+from pyrogram import Client, filters
+from EvaMaria.helpers import telegrapher
