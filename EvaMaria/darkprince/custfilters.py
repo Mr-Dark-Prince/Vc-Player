63ec556f65c42af3705a5e63f60ebd3e835028bd
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-24 10:34:21 +0530

    Create custfilters.py

---
+++ EvaMaria/darkprince/custfilters.py
@@ -0,0 +1,47 @@
+from pyrogram import (
+    filters
+)
+from config import (
+    SUDO_USERS
+)
+from EvaMaria.darkprince.admin_check import admin_check
+
+
+def f_sudo_filter(filt, client, message):
+    return bool(
+        message.from_user.id in SUDO_USERS
+    )
+
+
+sudo_filter = filters.create(
+    func=f_sudo_filter,
+    name="SudoFilter"
+)
+
+
+def onw_filter(filt, client, message):
+    if SUDO_USERS:
+        return bool(
+            True # message.from_user.id in SUDO_USERS
+        )
+    else:
+        return bool(
+            message.from_user and
+            message.from_user.is_self
+        )
+
+
+f_onw_fliter = filters.create(
+    func=onw_filter,
+    name="OnwFilter"
+)
+
+
+async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+
+admin_fliter = filters.create(
+    func=admin_filter_f,
+    name="AdminFilter"
+)
