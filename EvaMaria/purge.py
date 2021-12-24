0cf35aed81f1eb54e22bf649930e4989b70c4425
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-24 10:40:35 +0530

    Create purge.py

---
+++ EvaMaria/purge.py
@@ -0,0 +1,55 @@
+import asyncio
+from pyrogram import Client, filters
+
+from EvaMaria.darkprince.admincheck import admin_check
+from EvaMaria.darkprince.custfilters import f_onw_fliter
+
+TG_MAX_SELECT_LEN = 100
+
+@Client.on_message(
+    filters.command("purge") &
+    f_onw_fliter
+)
+async def purge(client, message):
+    """ purge upto the replied message """
+    if message.chat.type not in (("supergroup", "channel")):
+
+        return
+
+    is_admin = await admin_check(message)
+
+    if not is_admin:
+        return
+
+    status_message = await message.reply_text("...", quote=True)
+    await message.delete()
+    message_ids = []
+    count_del_etion_s = 0
+
+    if message.reply_to_message:
+        for a_s_message_id in range(
+            message.reply_to_message.message_id,
+            message.message_id
+        ):
+            message_ids.append(a_s_message_id)
+            if len(message_ids) == TG_MAX_SELECT_LEN:
+                await client.delete_messages(
+                    chat_id=message.chat.id,
+                    message_ids=message_ids,
+                    revoke=True
+                )
+                count_del_etion_s += len(message_ids)
+                message_ids = []
+        if len(message_ids) > 0:
+            await client.delete_messages(
+                chat_id=message.chat.id,
+                message_ids=message_ids,
+                revoke=True
+            )
+            count_del_etion_s += len(message_ids)
+
+    await status_message.edit_text(
+        f"deleted {count_del_etion_s} messages"
+    )
+    await asyncio.sleep(5)
+    await status_message.delete()
